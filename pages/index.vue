b909f6988d285676e18d17206f0241e9661ba970
mugayoshi <muga.y.j3039@gmail.com>
2020-05-10 20:13:54 +0900

    add data, computed, methods

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,80 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        parenting-support-service
-      </h1>
-      <h2 class="subtitle">
-        My dandy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-content>
+    <v-container>
+      <v-card>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title> 宅配お弁当 {{ str }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            {{ filterResponse }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      <v-list>
+        <v-subheader>REPORTS</v-subheader>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-container>
+  </v-content>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Vue from "vue";
 
+interface FactResponse {
+  id: string;
+  text: string;
+  type: string;
+  upvotes: number;
+  user: {
+    name: {
+      first: string;
+      last: string;
+    }
+  },
+}
 export default Vue.extend({
-  components: {
-    Logo
+  async asyncData({ app }){
+    const response = await app.$axios.$get('https://cat-fact.herokuapp.com/facts');
+    const arrays: FactResponse[] = response.all;
+    return {response, arrays}
+
+  },
+  data: () =>( {
+    response: '',
+    arrays: [],
+    item: 1,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Account", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" }
+    ],
+    str: "hola",
+    hours: new Date().getHours.toString
+  }),
+  computed: {
+    filterResponse: function(){
+      return JSON.stringify(this.arrays.slice(0, 3));
+    }
+  },
+  methods: {
+    getYear(): string {
+      return "hola";
+    }
   }
-})
+});
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
